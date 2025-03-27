ea94920b8d32f292d7f9accd4644997e14927953
Shay <shaghayegh.reza@gmail.com>
2025-03-27 12:39:56 -0400

    edition5

--- Main.py
+++ Main.py
@@ -19,7 +19,7 @@ from add_negatives import add_negative_samples_from_masterlist
 from fingerprint_extraction import extract_fingerprints
 from column_selection import select_final_columns
 
-def process_csv_files(data_path, masterlist_path, separated_files_dir, output_dir1, output_dir2, MasterList_Information, DesiredColumns):
+def process_csv_files(data_path, masterlist_path, separated_files_dir, output_dir1, output_dir2, output_dir3, MasterList_Information, DesiredColumns):
     """Processes all CSV files through data curation steps."""
 
     # Step 1: Separate protein-related data and store in subfolders
@@ -43,6 +43,7 @@ def process_csv_files(data_path, masterlist_path, separated_files_dir, output_di
             # Step 3-8: Process each separated file after computing scores
             for sep_file in separated_files:
                 sep_file_name = os.path.basename(sep_file)
+                base_name = os.path.splitext(sep_file_name)[0]
                 print(f"  Processing separated file: {sep_file_name}")
         
                 # Load separated file
@@ -61,13 +62,16 @@ def process_csv_files(data_path, masterlist_path, separated_files_dir, output_di
                 df = generate_ml_labels(df)
         
                 # Save curated CSV file (MLReady)
-                output_file1 = os.path.join(output_dir1, f"MLReady_{sep_file_name}")
-                df.to_csv(output_file1, index=False)
-        
-                print(f"  Saved intermediate file: {output_file1}")
+                output_file1_csv = os.path.join(output_dir1, f"MLReady_{base_name}.csv")
+                df.to_csv(output_file1_csv, index=False)
+
+                output_file1_parquet = os.path.join(output_dir1, f"MLReady_{base_name}.parquet")
+                df.to_parquet(output_file1_parquet, index=False)
+                
+                print(f"  Saved intermediate file: {output_file1_csv}")
         
                 # Step 7: Extract chemical fingerprints
-                #df = extract_fingerprints(df) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                df = extract_fingerprints(df) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         
         
                 # Step 8: RenameColumns
@@ -77,27 +81,41 @@ def process_csv_files(data_path, masterlist_path, separated_files_dir, output_di
                 df = select_final_columns(df, DesiredColumns)                
        
                 # Save as CSV
-                output_file2_csv = os.path.join(output_dir2, f"MLReadyPlusFPs_{sep_file_name}.csv")
+                output_file2_csv = os.path.join(output_dir2, f"MLReadyPlusFPs_{base_name}.csv")
                 df.to_csv(output_file2_csv, index=False)
                 
                 # Save as Parquet
-                output_file2_parquet = os.path.join(output_dir2, f"MLReadyPlusFPs_{sep_file_name}.parquet")
+                output_file2_parquet = os.path.join(output_dir2, f"MLReadyPlusFPs_{base_name}.parquet")
                 df.to_parquet(output_file2_parquet, index=False)
                 
                 print(f"Saved CSV: {output_file2_csv}")
                 print(f"Saved Parquet: {output_file2_parquet}")
+                #-----------------------
+                df = select_final_columns(df, DesiredColumns2)                
+       
+                # Save as CSV
+                output_file3_csv = os.path.join(output_dir3, f"MLReadyPlusFPs_{base_name}.csv")
+                df.to_csv(output_file3_csv, index=False)
+                
+                # Save as Parquet
+                output_file3_parquet = os.path.join(output_dir3, f"MLReadyPlusFPs_{base_name}.parquet")
+                df.to_parquet(output_file3_parquet, index=False)
+                print(f"Saved CSV: {output_file3_csv}")
+                print(f"Saved Parquet: {output_file3_parquet}")
+                
 
-def main(data_path, masterlist_path, separated_files_dir, output_dir1, output_dir2, MasterList_Information, DesiredColumns):
+
+def main(data_path, masterlist_path, separated_files_dir, output_dir1, output_dir2, output_dir3, MasterList_Information, DesiredColumns):
     """Main function to execute the full data curation pipeline."""
     os.makedirs(separated_files_dir, exist_ok=True)
     os.makedirs(output_dir1, exist_ok=True)
     os.makedirs(output_dir2, exist_ok=True)
 
-    process_csv_files(data_path, masterlist_path, separated_files_dir, output_dir1, output_dir2, MasterList_Information, DesiredColumns)
+    process_csv_files(data_path, masterlist_path, separated_files_dir, output_dir1, output_dir2, output_dir3, MasterList_Information, DesiredColumns)
 
 if __name__ == "__main__":
     # Define paths (Modify as needed)
-    path=r"D:\0000-UHN\03-DataAndCodes\Data\ASMS\EASMS-7March"
+    path=r"D:\0000-UHN\03-DataAndCodes\Data\ASMS\EASMS-20March"
     data_path = os.path.join(path, "RawData0")
     masterlist_path = os.path.join(path, "MasterLists")  
     MasterList_Information = os.path.join(masterlist_path, "MasterList_Information.xlsx")   
@@ -105,52 +123,8 @@ if __name__ == "__main__":
     separated_files_dir = os.path.join(path, "Separated_Files")                                 
     output_dir1 = os.path.join(path, "MLReady") 
     output_dir2 = os.path.join(path, "MLReady_Plus_FPs")
+    output_dir3 = os.path.join(path, "MLReady_Plus_FPs_2")
 
-   ''' DesiredColumns = ['ASMS_BATCH_NUM',
-     'COMPOUND_ID',
-     'COMPOUND_FORMULA',
-     'SMILES',
-     'POOL_NAME',
-     'POOL_ID',
-     'POOL_SIZE',
-     'PROTEIN_NUMBER',
-     'TARGET_ID',
-     'PROTEIN_ID',
-     'PROTEIN_SEQ',
-     'PROTEIN_TAG',
-     'INCUBATION_VOLUME',
-     'PROTEIN_CONC',
-     'COMPOUND_CONC',
-     'MS_REPRODUCABILITY',
-     'POS_INT_REP1',
-     'POS_INT_REP2',
-     'POS_INT_REP3',
-     'TARGET_VALUE',
-     'SELECTIVE_VALUE',
-     'NTC_VALUE',
-     'ENRICHMENT',
-     'SELECTIVE_ENRICHMENT',
-     'PVALUE',
-     'BINARY_LABEL',
-     'SPR',
-     'KD',
-     'ISOMERS',
-     'MassSpec_Detected',
-     'EASMS_ENRICHMENT',
-     'MEAN_NONTARGET_VALUES',
-     'LABEL',
-     'MW',
-     'ALOGP',
-     'ECFP4',
-     'ECFP6',
-     'FCFP4',
-     'FCFP6',
-     'MACCS',
-     'RDK',
-     'AVALON',
-     'TOPTOR',
-     'ATOMPAIR']'''
-    
     DesiredColumns = ['ASMS_BATCH_NUM',
      'COMPOUND_ID',
      'COMPOUND_FORMULA',
@@ -195,5 +169,36 @@ if __name__ == "__main__":
      'AVALON',
      'TOPTOR',
      'ATOMPAIR']
+    
+    DesiredColumns2 = ['ASMS_BATCH_NUM',
+     'COMPOUND_ID',
+     'COMPOUND_FORMULA',
+     'SMILES',
+     'PROTEIN_NUMBER',
+     'TARGET_ID',
+     'PROTEIN_ID',
+     'PROTEIN_SEQ',
+     'POS_INT_REP1',
+     'POS_INT_REP2',
+     'POS_INT_REP3',
+     'TARGET_VALUE',
+     'PVALUE',
+     'BINARY_LABEL',
+     'ISOMERS',
+     'MassSpec_Detected',
+     'EASMS_ENRICHMENT',
+     'MEAN_NONTARGET_VALUES',
+     'LABEL',
+     'MW',
+     'ALOGP',
+     'ECFP4',
+     'ECFP6',
+     'FCFP4',
+     'FCFP6',
+     'MACCS',
+     'RDK',
+     'AVALON',
+     'TOPTOR',
+     'ATOMPAIR']
 
-    main(data_path, masterlist_path, separated_files_dir, output_dir1, output_dir2, MasterList_Information, DesiredColumns)
+    main(data_path, masterlist_path, separated_files_dir, output_dir1, output_dir2, output_dir3, MasterList_Information, DesiredColumns)
